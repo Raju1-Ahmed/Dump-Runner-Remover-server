f9b04b703685b414695affe7013c4e4e91fb24e7
Raju1-Ahmed <robiussaniraju@gmail.com>
2026-09-22 11:01:30 +0600

    Allow renamed Netlify frontend origin

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const allowedOrigins = new Set([
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'https://papaya-pixie-50b854.netlify.app',
+  'https://dumprunner.netlify.app',
   ...(process.env.CLIENT_ORIGIN || '').split(',').map((origin) => origin.trim()).filter(Boolean),
 ]);
 
